4dd65456f78f3dc135d758e399639ff86ad181fa
akshitha111 <akshitha2408@msitprogram.net>
2018-08-27 23:20:12 +0530

    name

---
+++ assignment 1/Assignment-1/Solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Solution {
+	public static void main(String[] args) {
+		String name;
+		int rollNumber;
+		Scanner s = new Scanner(System.in);
+		name = s.nextLine();
+		rollNumber = s.nextInt();
+
+		System.out.println("Name : " +name+ " Roll Number : " +rollNumber);
+	}
+}
