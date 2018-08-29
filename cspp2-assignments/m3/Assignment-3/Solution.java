85e13bfc454457a5f88f56349b6f4f834c42773a
akshitha111 <akshitha2408@msitprogram.net>
2018-08-29 14:51:54 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m3/Assignment-3/Solution.java
+++ cspp2-assignments/m3/Assignment-3/Solution.java
@@ -1,35 +1,42 @@
-
 import java.util.Scanner;
-/*
-	Do not modify this main function.
-	*/
-public class Solution {
+/** Do not modify this main function.
+*/
+public final class Solution {
+    /** This is class.
+    */
+    private Solution() {
+    /** This is constructor.
+    */
+    }
 
-    public static void main(String[] args) {
+/**
+* Class for solution.
+* @param args String
+*/
+    public static void main(final String[] args) {
 
-        Scanner s=new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
-        gcd(n1,n2);
-        System.out.println(gcd(n1,n2));
+        gcd(n1, n2);
+        System.out.println(gcd(n1, n2));
     }
-    /*
-	Need to write the gcd function and print the output.
-	*/
-    public static int gcd(int n1, int n2) {
-        //int n1 = 81, n2 = 153, gcd = 1;
-        if(n1==n2) {
+    /**
+    Need to write the gcd function and print the output.
+    *@param n1 int
+    *@param n2 int
+    *@return int
+    */
+    public static int gcd(final int n1, final int n2) {
+        if (n1 == n2) {
             return n1;
         }
-        if(n1 == 0 || n2 ==0) {
+        if (n1 == 0 || n2 == 0) {
             return 0;
         }
-        if(n1>n2) {
-            return gcd(n1-n2,n2); 
-            }           
-        return gcd(n1,n2-n1);
-            
-        }
-
+        if (n1 > n2) {
+            return gcd(n1 - n2, n2);
+            }
+        return gcd(n1, n2 - n1);
     }
-
+}
