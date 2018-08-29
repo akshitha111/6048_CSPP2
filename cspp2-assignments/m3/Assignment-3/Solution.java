fb95b7bf1533090c637bfc92c94ec5a0deeb8b33
akshitha111 <akshitha2408@msitprogram.net>
2018-08-29 12:20:50 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m3/Assignment-3/Solution.java
+++ cspp2-assignments/m3/Assignment-3/Solution.java
@@ -11,9 +11,25 @@ public class Solution {
         int n1 = s.nextInt();
         int n2 = s.nextInt();
         gcd(n1,n2);
-        System.out.println(gcd);
+        System.out.println(gcd(n1,n2));
     }
     /*
 	Need to write the gcd function and print the output.
 	*/
-}
+    public static int gcd(int n1, int n2) {
+        //int n1 = 81, n2 = 153, gcd = 1;
+        if(n1==n2) {
+            return n1;
+        }
+        if(n1 == 0 || n2 ==0) {
+            return 0;
+        }
+        if(n1>n2) {
+            return gcd(n1-n2,n2); 
+            }           
+        return gcd(n1,n2-n1);
+            
+        }
+
+    }
+
