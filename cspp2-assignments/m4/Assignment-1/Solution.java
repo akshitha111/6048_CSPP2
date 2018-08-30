a0287b698339cbaa08c34088adc634ae9b0949a5
akshitha111 <akshitha2408@msitprogram.net>
2018-08-30 19:16:00 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-1/Solution.java
+++ cspp2-assignments/m4/Assignment-1/Solution.java
@@ -9,7 +9,7 @@ public class Solution {
         int n = sc.nextInt();
         int a[] = new int[n];
         for (int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();        
+            a[i] = sc.nextInt();     
         }
         int max = a[0];
         for (int i = 0; i < n; i++) {
@@ -17,7 +17,6 @@ public class Solution {
                 max = a[i];
             }
             }
-        
         System.out.println(max);
     }
     }
