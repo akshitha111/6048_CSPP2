ccc89197c51f1b11bd83c77fc922263fc4c0379b
akshitha111 <akshitha2408@msitprogram.net>
2018-08-31 14:02:37 +0530

    "check_style_score -> 1 of 1 passed. style score: 1/1 "

--- cspp2-assignments/m4/Assignment-3/Solution.java
+++ cspp2-assignments/m4/Assignment-3/Solution.java
@@ -1,29 +1,56 @@
+/**
+* binary.
+*/
 import java.util.Scanner;
-public class Solution
-{/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
-		for(int i=0;i<n;i++){
-			String s=sc.next();
-			String res=binaryToDecimal(s);//Write binaryToDecimal function
-			System.out.println(res);
-		}
-	}
-	public static String binaryToDecimal(String s) {
-		int decimal = 0;
-		int i =0;
-		long n = Long.parseLong(s);
-	    while(n!=0) {
-	    		decimal += ((n%10)*Math.pow(2,i));
-	    		n = n/10;
-	    		i++;
-	    		}
-	    		String str1 = decimal + "";
-	    		return str1;
-		}	
+/**
+* import.
+*/
+public final class Solution {
+/**
+*Do not modify this main function.
+*/
+public static final int N_U = 10;
+/**
+ * Constructs the object.
+ */
+    private Solution() {
+        /**
+         * constructor.
+         */
+    }
+/**
+ * { function_description }
+ * main function.
+ * @param      args  The arguments
+ */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+            String res = binaryToDecimal(s);
+            System.out.println(res);
+        }
+    }
+    /** .
+     * { function_description }
+     *
+     * @param      s     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+
+    public static String binaryToDecimal(final String s) {
+        int decimal = 0;
+        int i = 0;
+        long n = Long.parseLong(s);
+        while (n != 0) {
+                decimal += ((n % N_U) * Math.pow(2, i));
+                n = n / N_U;
+                i++;
+                }
+                String str1 = decimal + "";
+                return str1;
+        }
 
 }
