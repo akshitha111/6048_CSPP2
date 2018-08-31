cb433f019f85cdcf5df7b81591c73583fc1b3bdd
akshitha111 <akshitha2408@msitprogram.net>
2018-08-31 11:04:20 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- cspp2-assignments/m4/Assignment-4/Solution.java
+++ cspp2-assignments/m4/Assignment-4/Solution.java
@@ -1,32 +1,33 @@
 import java.util.Scanner;
-/* reverse.
+/**
+* reverse.
 */
 public final class Solution {
 /**
-	Do not modify the main function. 
-	*/
-	private Solution() {
-		/** constructor.
-		*/
-	}
-	/** class.
-	* @param args String
-	*/  
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		String reverse = reverseString(s);	
-		System.out.println(reverse);		
-	}
-	/** function.
-	* @param a String
-	* @return String
-	*/
-	public static String reverseString(String a) {
-		String str = "";
-		for(int k = 0; k < a.length(); k += 1) {
-			str = a.charAt(k) + str;
-		}
-		return str;
-	}
+Do not modify the main function.
+*/
+    private Solution() {
+        /** constructor.
+        */
+    }
+    /** class.
+    * @param args String
+    */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);
+        System.out.println(reverse);
+    }
+    /** function.
+    * @param a String
+    * @return String
+    */
+    public static String reverseString(final String a) {
+        String str = "";
+        for (int k = 0; k < a.length(); k += 1) {
+            str = a.charAt(k) + str;
+        }
+        return str;
+    }
 }
