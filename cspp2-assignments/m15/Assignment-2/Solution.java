f6b58cf7e9c3748f5c32c69403935d4e0ef56215
akshitha111 <akshitha2408@msitprogram.net>
2018-09-11 16:17:32 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m15/Assignment-2/Solution.java
+++ cspp2-assignments/m15/Assignment-2/Solution.java
@@ -204,8 +204,8 @@ public final class Solution {
                 }
             } catch(Exception e) {
                 System.out.println(e.getMessage());
-                break;
-            }
+                
+            }break;
             case "headSet":
                 if (tokens.length != 2) {
                     break;
