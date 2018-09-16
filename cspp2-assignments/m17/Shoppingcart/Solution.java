aaeb1f09fa06bf6c4482b02bbab15a232bc3a7fb
akshitha111 <akshitha2408@msitprogram.net>
2018-09-16 21:20:03 +0530

    "check_style_score -> 1 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m17/Shoppingcart/Solution.java
+++ cspp2-assignments/m17/Shoppingcart/Solution.java
@@ -124,7 +124,7 @@ class ShoppingCart {
                  if(cart.get(i).getProductName().equals(catalog.get(j).getProductName())){
                     
                     payableAmount += (cart.get(i).getQuantity() * catalog.get(j).getUnitPrice());
-                    payableAmount = totalAmount + (totalAmount * 0.15);
+                    payableAmount = getTotalAmount() + (totalAmount * 0.15);
                    
                 }       
     }
