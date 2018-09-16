d2be24b21854ec26889142577130c4e07855435a
akshitha111 <akshitha2408@msitprogram.net>
2018-09-16 21:29:16 +0530

    "check_style_score -> 1 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m17/Shoppingcart/Solution.java
+++ cspp2-assignments/m17/Shoppingcart/Solution.java
@@ -134,7 +134,7 @@ class ShoppingCart {
 
     public void applyCoupon(final String coupon) {
 
-        if (isCouponApplied) {
+        if (!isCouponApplied) {
             if (coupon.equals("IND10")) {
             couponCode = 0.1f;
             } else if (coupon.equals("IND20")) {
