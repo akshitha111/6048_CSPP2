1e5d9dd23dee271429f59365aabcc67b0abbde25
akshitha111 <akshitha2408@msitprogram.net>
2018-09-17 13:47:34 +0530

    "check_style_score -> 6 of 13 passed. style score: 0/1 "

--- cspp2-assignments/m20/Assignment-1/Solution.java
+++ cspp2-assignments/m20/Assignment-1/Solution.java
@@ -248,7 +248,7 @@ public final class Solution {
             tokens = sc.split(":");
         }
         String[] choices = tokens[1].split(",");
-            if(q < 4) {
+            if(q < 0) {
             	throw new Exception("Quiz does not have questions");
             }
             if(tokens.length < 5) {
