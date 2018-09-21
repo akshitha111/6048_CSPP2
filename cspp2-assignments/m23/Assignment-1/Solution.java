dc8ff0ccd67163c35fa586ca0f0ba1a234053f09
akshitha111 <akshitha2408@msitprogram.net>
2018-09-21 13:44:05 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m23/Assignment-1/Solution.java
+++ cspp2-assignments/m23/Assignment-1/Solution.java
@@ -95,13 +95,13 @@ class Solution {
 			for (int j = 0; j < length; j++) {
 				matrix[i][j] = BagOfWords.compareText(BagOfWords.fileToString(listOfFiles[i]),BagOfWords.fileToString(listOfFiles[j]));
 			}
-		}System.out.println("		\t");
+		}System.out.print("		\t");
 			for (int i = 0; i < listOfFiles.length-1; i++) {
-				System.out.print(""  + listOfFiles[i].getName());
+				System.out.print("\t"  + listOfFiles[i].getName());
 			}
-			System.out.println("" + listOfFiles[length-1].getName());
+			System.out.println("\t" + listOfFiles[length-1].getName());
 			for(int i = 0; i < length; i++) {
-				System.out.print(listOfFiles[i].getName() + "	");
+				System.out.print(listOfFiles[i].getName() + "\t");
 			for (int j = 0; j < length; j++) {
 				System.out.print(matrix[i][j] + "		");
 			}
