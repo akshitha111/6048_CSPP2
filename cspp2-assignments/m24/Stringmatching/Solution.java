058baf14b5f7f141569fb883968061e057a39ffa
akshitha111 <akshitha2408@msitprogram.net>
2018-09-24 10:50:26 +0530

    "check_style_score -> 1 of 3 passed. style score: 1/1 "

--- cspp2-assignments/m24/Stringmatching/Solution.java
+++ cspp2-assignments/m24/Stringmatching/Solution.java
@@ -1,217 +1,127 @@
-import java.io.BufferedReader;
 import java.io.File;
+import java.util.Scanner;
+import java.util.NoSuchElementException;
 import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Map;
-import java.util.*;
-import java.io.*;
-
-// class BagOfWords {
-// 	Map<String, Integer> dictionary1 = new HashMap<String, Integer>();
-// 	Map<String, Integer> dictionary2 = new HashMap<String, Integer>();
-// 	Map<String, List<Integer>> dictionary3 = new HashMap<String, List<Integer>>();
-// 	BagOfWords() {
-// 	}
-
-// 	public void addDic1(String[] list) {
-//         for(String i : list) {
-//          	if(dictionary1.containsKey(i)) {
-//          		int k = dictionary1.get(i);
-//          		dictionary1.put(i, k+1);
-//          	}
-//          	else {
-//          		dictionary1.put(i, 1);
-//          	}
-//         }
-//         // for(String key: dictionary1.keySet())
-//         // System.out.println(key+" "+dictionary1.get(key));
-// 	}
-// 	public void addDic2(String[] list) {
-//         for(String i : list) {
-//          	if(dictionary2.containsKey(i)) {
-//          		int k = dictionary2.get(i);
-//          		dictionary2.put(i, k+1);
-//          	}
-//          	else {
-//          		dictionary2.put(i, 1);
-//          	}
-//         }
-//         // for(String key: dictionary2.keySet())
-//         // System.out.println(key+" "+dictionary2.get(key));
-// 	}
-
-// 	public double getDistance() {
-// 		float num = 0;
-// 		float a = 0;
-// 		double den = 0;
-//         double res;
-// 		for(String i:dictionary1.keySet()){
-// 			if(dictionary2.containsKey(i)) {
-//                 num += dictionary1.get(i)*dictionary2.get(i);
-// 			}
-// 		}
-// 		for(String i:dictionary1.keySet()) {
-//             a += Math.pow(dictionary1.get(i), 2);
-// 		}
-// 		float c = 0;
-// 		for(String i:dictionary2.keySet()) {
-//             c += Math.pow(dictionary2.get(i), 2);
-// 		}
-// 		den = Math.sqrt(a)*Math.sqrt(c);
-// 		res = (num/den)*100;
-// 		//System.out.print("HHHHHH"+res);
-//         if (Double.isNaN(res)) {
-//         	return 0;
-//         }
-//         else {
-//         	return res;
-//         }
-// 	}
-// }
-
-class StringMatching {
-	String l1;
-	String l2;
-	int len1;
-	int len2;
-	StringMatching() {
-
-	}
-	public void str1(String tokens) {
-		l1 = tokens;
-		len1 = tokens.length();
-		// System.out.println(len1);
-	}
-	public void str2(String tokens1) {
-		l2 = tokens1;
-		len2 = tokens1.length();
-		// System.out.println(len2);
-	}
-	 public double compare() {
-	 	double total = len1+len2;
-	 	double max = 0;
-        for(int i = 0; i<l1.length(); i++) {
-        	for(int j = 0; j<l2.length(); j++) {
-        		int k = 0;
-        		if(l1.charAt(i) == l2.charAt(j)) {
-        			double c = 1;
-        			while((k+i)<l1.length() && (k+j)<l2.length()) {
-        				if(l1.charAt(i+k) == l2.charAt(j+k)) {
-        					c++;
-        					if(c>max) {
-        						max = c;
-        					}
-        				}
-        				else{
-        					c = 0;
-        					break;
-        				}
-        				k++;
-        			}
-        		}
-        	}
-        }
-        // System.out.println(max);
-        double res = (max*2)/total;
-        double res1 = res*100;
-        return res1;
-	}
-}
+import java.io.FileNotFoundException;
 /**
- * Class for solution..
+ * Class for substring.
  */
+class Substring {
+    /**
+     * constructor for substring method.
+     */
+    Substring() {
 
-class Solution {
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args         The arguments
-	 *
-	 * @throws     IOException  { exception_description }
-	 */
-	/**
-	 * Constructs the object.
-	 */
-	Solution() {
-
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args         The arguments
-	 *
-	 * @throws     IOException  { exception_description }
-	 */
-	public static void main(final String[] args) throws IOException {
-		double max = 0;
-		int a = 0;
-		int b = 0;
-		// StringMatching match = new StringMatching();
-        Scanner input = new Scanner(System.in);
+    }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @param      givenFile  The given file
+     *
+     * @return     String representation of the object.
+     */
+    public String toString(final File givenFile) {
+        String result = "";
         try {
-        	String x = input.nextLine();
-        	File file = new File(x);
-			File[] files = file.listFiles();
-			System.out.print("             ");
-			for (int i = 1; i <= files.length; i++) {
-				System.out.print("file" + i + ".txt" + "    ");
-			}
-			System.out.println();
-
-		    for (int i = 0; i < files.length; i++) {
-		    	int h = i + 1;
-                   System.out.print("file" + h + ".txt");
-		    	for (int j = 0; j < files.length; j++) {
-
-                        StringMatching match = new StringMatching();
-                        try {
-                        	Scanner s = new Scanner(files[i]);
-		    			String line = s.useDelimiter("\\A").next();
-		    			match.str1(line);
-		    			String[] tokens = line.toLowerCase().split(" ");
-
-                        } catch (NoSuchElementException e) {
-        	                System.out.print("         0.0");
-                        }
-                        catch (NullPointerException e) {
-                        	System.out.print("");
-                        }
-                        try {
-                        	Scanner s1 = new Scanner(files[j]);
-		    			String line1 = s1.useDelimiter("\\A").next();
-		    		    match.str2(line1);
-		    			String[] tokens1 =
-		    			line1.toLowerCase().split(" ");
-
-		    			double p = match.compare();
-		    			System.out.print("        ");
-		    			System.out.format("%.1f", p);
-
-		    			if (p > max && i != j) {
-		    				 a = i + 1;
-		    				 b = j + 1;
-		    				 max = p;
-		    			    }
-
-                        }
-                        catch (NoSuchElementException e) {
-        	                System.out.print("        0.0");
-                        }
-                        catch (NullPointerException e) {
-                        	System.out.print("");
-                        }
-
-
-		    	}
-		    	System.out.println();
-		    }
-		    System.out.println("Maximum similarity is between " + "file" + a + ".txt and file" + b + ".txt");
+            Scanner s = new Scanner(new FileReader(givenFile));
+            StringBuilder sb = new StringBuilder();
+            while (s.hasNext()) {
+                sb.append(s.next());
+                sb.append(" ");
             }
-            catch(NoSuchElementException e) {
-            	System.out.println("Empty Directory");
+            s.close();
+            result = sb.toString();
+        } catch (FileNotFoundException e) {
+            System.out.println("no file");
+        }
+        return result;
+    }
+    /**
+     * method to find longest common substring.
+     *
+     * @param      firstString   The first string
+     * @param      secondString  The second string
+     *
+     * @return     returns the lcs value.
+     */
+    public double findLCS(final String firstString, final String secondString) {
+    int lengthOne = firstString.length();
+    int lengthTwo = secondString.length();
+    double totalLength = lengthOne + lengthTwo;
+    int[][] tempMatrix = new int[lengthOne + 1][lengthTwo + 1];
+    double result = 0;
+    double lcsValue = 0;
+    for (int i = 0; i <= lengthOne; i++) {
+        for (int j = 0; j <= lengthTwo; j++) {
+            if (i == 0 || j == 0) {
+                tempMatrix[i][j] = 0;
+            } else if (firstString.charAt(i - 1)
+             == secondString.charAt(j - 1)) {
+                tempMatrix[i][j] =
+                 tempMatrix[i - 1][j - 1] + 1;
+            } else {
+                tempMatrix[i][j] = 0;
+            }
+            if (result < tempMatrix[i][j]) {
+                result = tempMatrix[i][j];
             }
         }
     }
+    final int hundred = 100;
+    lcsValue = ((result * 2) / totalLength) * hundred;
+    // System.out.println((int)lcsValue);
+    return Math.round(lcsValue * hundred) / hundred;
+    }
+}
+/**
+ * Class for solution.
+ */
+class Solution {
+    /**
+     * constructor for solution class.
+     */
+    protected Solution() {
+
+    }
+    /**
+     * main method for solution class.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        try {
+        Substring s = new Substring();
+        String path;
+        Scanner scan = new Scanner(System.in);
+        path = scan.nextLine();
+        File folder = new File(path);
+        File[] filesList = folder.listFiles();
+        int length = filesList.length;
+        double[][] matrix = new double[length][length];
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                matrix[i][j] = s.findLCS(
+                    s.toString(filesList[i]),
+                     s.toString(filesList[j]));
+            }
+        }
+        System.out.print("     \t");
+        for (int i = 0; i < filesList.length - 1; i++) {
+            System.out.print("\t" + filesList[i].getName());
+        }
+        System.out.println("\t" + filesList[length - 1].getName());
+        for (int i = 0; i < length; i++) {
+            System.out.print(filesList[i].getName() + "\t");
+            for (int j = 0; j < length; j++) {
+                System.out.print(
+                    matrix[i][j] + "        ");
+            }
+            System.out.println();
+        }
+    } catch (NoSuchElementException e) {
+        System.out.println("Empty Directory");
+    }
+
+    }
+}
 
